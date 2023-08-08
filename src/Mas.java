25c0fbc1f46ce3ee194eef1f9006f49a3a77b879
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-08-08 21:48:48 +0300

    fgf

--- src/Mas.java
+++ src/Mas.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Mas {
+public class Mas {
+    public static void main(String[] args) {
+        int max = 0;
+        // int min = 0;
+        // int max = mas [0];
+        int[] mas = {25, 47, 34, 18, 96, 33, 28, 55, 87, 13};
+            for (int i = 1; i < mas.length; i++) {
+                // if (min > mas[i]);
+                // min = mas[i];
+            if (max < mas[i]) ;
+            max = mas[i];
+        }
+        System.out.println("Max is " + max);
+            //System.out.println("Min is " + min);
+    }
 }
